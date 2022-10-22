7a0cea7c54ef098ddfb3e4c337907d0d92e55a3d
H <hai_meimei@outlook.com>
2022-10-23 00:22:35 +0800

    demu is replaced with rofi

--- config.h
+++ config.h
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun",  NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
