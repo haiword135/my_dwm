6c8da0452549b67a6e8e257ca8d5637dc068a28e
H <hai_meimei@outlook.com>
2023-01-01 23:25:14 +0800

    patch viewontag

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const int vertpad            = 0;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
 static const char *fonts[]          = { "CaskaydiaCove Nerd Font:style=medium:size=15",
